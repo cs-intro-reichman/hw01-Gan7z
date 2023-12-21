4448654b44044cbdbff58ae8ea154d3c5c15868e
Gan7z <oshri.gantz@post.runi.ac.il>
2023-12-21 19:51:04 +0200

    git add . git commit -m "Add awesome code for Homework 1" git push origin main

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,23 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+
+import java.util.Random;
+
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int res1 = (int)((Math.random() * (b - a)) + a );
+		int res2 = (int)((Math.random() * (b - a)) + a );
+		int res3 = (int)((Math.random() * (b - a)) + a );
+		int res = Math.min(res1, Math.min(res2, res3));
+
+		System.out.println(res1+"\n"+res2+"\n"+res3+"\nThe minimal generated number was "+res);
+
+		
+
+
+
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,15 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		boolean check = false;
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if (a+c > b && a+b > c && b+c > a)
+		{
+			check = true;
+		}
+		System.err.println(a+", "+b+", "+c+": "+check);
+
 	}
 }
