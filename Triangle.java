8af3228922a6158f8fbb8a4dcfcea151c5be3426
Gan7z <oshri.gantz@post.runi.ac.il>
2023-12-21 20:00:57 +0200

    git add . git commit -m "Add awesome code for Homework 1" git push origin main

--- Triangle.java
+++ Triangle.java
@@ -5,14 +5,11 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		boolean check = false;
+		
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
-		if (a+c > b && a+b > c && b+c > a)
-		{
-			check = true;
-		}
+		boolean check = (a+c > b && a+b > c && b+c > a);
 		System.err.println(a+", "+b+", "+c+": "+check);
 
 	}
